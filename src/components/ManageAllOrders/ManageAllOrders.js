8ba01a81013aa8b1bd5f2208511bbe997cd959fe
saifulislammunna <saifulmunna28@gmail.com>
2021-10-30 18:05:14 +0600

    update

--- src/components/ManageAllOrders/ManageAllOrders.js
+++ src/components/ManageAllOrders/ManageAllOrders.js
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 
 const ManageAllOrders = () => {
-   /*  const [services, setServices] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:5000/services')
-        .then(res => res.json())
-        .then(data => setServices(data))
-     },[]) */
+    const {user } = useAuth();
+     
     return (
         <div>
             <h1>This is manage all orders</h1>
-            {/* <h1>This is manage order</h1>
-            {
-                services.map(service => <div key={service._id}>
-                    <h3>{service.name}</h3>
-                    <button  >Delete</button>
-
-                </div>)
-            } */}
+             <div>
+               {
+                   user.email? <h1>All Users</h1> : ''
+               }
+          
+          < p  className="text-dark  ">
+       
+           {user.displayName}  
+     
+          </p>
+         
+             </div>
         </div>
     );
 };
---
+++ src/components/Users/Users.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+
+const Users = () => {
+    /*    const [users,setUsers] = useState(); 
+
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+        .then(res => res.json())
+        .then(data => setUsers(data))
+     },[])  */
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Users;
