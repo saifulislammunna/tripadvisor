b448dc3e00e568d7a75522c7d1e146682ca8c1f2
saifulislammunna <saifulmunna28@gmail.com>
2021-10-31 17:20:08 +0600

    added

--- src/components/ManageAllOrders/ManageAllOrders.js
+++ src/components/ManageAllOrders/ManageAllOrders.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
  
 const ManageAllOrders = () => {
     const [orders, setOrders] = useState([]);
@@ -10,6 +11,23 @@ const ManageAllOrders = () => {
         .then(data => setOrders(data))
      },[]);
      
+     const handleDelete = _id => {
+        const url =  `https://afternoon-wave-38333.herokuapp.com/orders/${_id}`
+        fetch(url,{
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data); 
+           if(data.deletedCount){
+                alert('Are you sure')
+                const remaining = orders.filter(service => service._id !== _id);
+                setOrders(remaining);
+ 
+          } 
+         
+        })
+    };
     return (
         <div>
              
@@ -18,7 +36,8 @@ const ManageAllOrders = () => {
                 orders.map(order => <div key={order._id} className="d-flex justify-content-center align-items-center">
                     <h3 className="p-2">UserName:{order.name}</h3>
                      <p className="p-3">Order Id:{order._id}</p> 
-                     <p>Status : Pending.. <button className="btn-primary rounded">Update</button></p>
+                     <p>Status : Pending.. <button className="btn-primary rounded">Update</button>  <Button onClick={() => handleDelete(order._id)} className="btn-danger">Delete User</Button></p>
+                    
                     
                 </div>)
             }
